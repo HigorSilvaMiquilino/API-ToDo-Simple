e18a4d7645ff20db96ae098e06b7032b9ee1fc4c
HigorSilvaMiquilino <higor.miquilino@gmail.com>
2023-05-29 20:18:00 -0300

    Extension recommendations

--- src/main/java/com/praticando/todosimple/DemoApplication.java
+++ src/main/java/com/praticando/todosimple/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.praticando.demo;
+package com.praticando.todosimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
