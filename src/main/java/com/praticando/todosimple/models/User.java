92579fc9a8ecbbf4b26e0e00a8a933cb93b53e73
HigorSilvaMiquilino <higor.miquilino@gmail.com>
2023-05-31 00:20:49 -0300

    Create UserController

--- src/main/java/com/praticando/todosimple/models/User.java
+++ src/main/java/com/praticando/todosimple/models/User.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -88,6 +87,7 @@ public class User {
         this.password = password;
     }
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return this.tasks;
     }
